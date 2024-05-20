f925f794cdc974a3bb4dc74e26c63287308629d7
evalshine <dankupr21@gmail.com>
2024-05-20 11:44:23 +0300

    init

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 20;
+    int y = 50;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
