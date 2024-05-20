20d60a31f09b3a3219958018fad1e62b7eb1c712
evalshine <dankupr21@gmail.com>
2024-05-20 11:51:16 +0300

    init

--- swap.c
+++ swap.c
@@ -8,10 +8,6 @@ int main()
     printf("x is %i\n", x);
     printf("y is %i\n", y);
 
-    int temp;
-    x = y;
-    y = temp;
-
 
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
