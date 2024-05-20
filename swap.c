1fa01ae868c5343871dec271b304beec428c9c02
evalshine <dankupr21@gmail.com>
2024-05-20 11:47:37 +0300

    add lines

--- swap.c
+++ swap.c
@@ -8,6 +8,10 @@ int main()
     printf("x is %i\n", x);
     printf("y is %i\n", y);
 
+    int temp;
+    x = y;
+    y = temp;
+
 
     printf("x is now %i\n", x);
     printf("y is now %i\n", y);
